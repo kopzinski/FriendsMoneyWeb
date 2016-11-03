ebbf15e697fe0dd20628c1a4b82a0949b9dbf63f
Paulo Eduardo Kopzinski Mello <eduardo@kopzinski.com.br>
2016-11-03 12:25:52 -0200

    Simple server to open at port 8080

---
+++ app/routes.js
@@ -0,0 +1,5 @@
+module.exports = function (app) {
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+};
